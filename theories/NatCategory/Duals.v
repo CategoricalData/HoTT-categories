f00b6574c269bec506b2937bec0c1cea31d6eaa3
Jason Gross <jgross@mit.edu>
2013-07-24 12:53:56 -0400

    Update nat category duals
    After    | File Name                             | Before
    -----------------------------------------------------------
    0m39.36s | Category/Equality                     | 0m39.79s
    0m21.15s | NaturalTransformation/Equality        | 0m21.71s
    0m16.96s | Functor/Equality                      | 0m17.31s
    0m10.92s | Comma/Duals                           | 0m10.10s
    0m10.83s | Comma/CommaCategory                   | 0m10.92s
    0m09.60s | Groupoid                              | 0m09.08s
    0m07.37s | Category/Morphisms                    | 0m08.00s
    0m04.61s | Grothendieck/ToSet                    | 0m04.63s
    0m03.90s | Groupoid/Functors                     | 0m03.76s
    0m02.32s | SumPreCategory                        | 0m02.38s
    0m02.13s | Category/Product                      | 0m02.08s
    0m02.12s | Functor/CompositionLaws               | 0m02.08s
    0m01.74s | NaturalTransformation/Duals           | 0m01.72s
    0m01.41s | NaturalTransformation/Composition     | 0m01.33s
    0m01.23s | NaturalTransformation/CompositionLaws | 0m01.23s
    0m01.10s | InitialTerminalCategory               | 0m01.18s
    0m01.02s | Comma/Projection                      | 0m01.04s
    0m00.93s | NatCategory                           | 0m01.02s
    0m00.68s | BoolCategory                          | 0m00.64s
    0m00.56s | SetCategory                           | 0m00.48s
    0m00.44s | Discrete/Duals                        | 0m00.42s
    0m00.39s | Functor/Product                       | 0m00.34s
    0m00.34s | Common                                | 0m00.31s
    0m00.33s | IndiscreteCategory                    | 0m00.28s
    0m00.28s | NaturalTransformation/Identity        | 0m00.25s
    0m00.27s | Adjoint/UnitCounit                    | 0m00.28s
    0m00.26s | Functor/Composition                   | 0m00.24s
    0m00.24s | Category/Objects                      | 0m00.20s
    0m00.24s | NatCategory/Duals                     | 0m00.74s
    0m00.23s | FunctorPreCategory                    | 0m00.24s
    0m00.20s | Functor/Duals                         | 0m00.24s
    0m00.19s | IsGroupoid                            | 0m00.22s
    0m00.18s | NaturalTransformation/Core            | 0m00.18s
    0m00.16s | Category/Core                         | 0m00.19s
    0m00.16s | NaturalTransformation/Product         | 0m00.15s
    0m00.15s | Functor/Core                          | 0m00.13s
    0m00.15s | Category/Duals                        | 0m00.16s
    0m00.11s | ComputableCat                         | 0m00.11s
    0m00.11s | Category/Category                     | 0m00.11s
    0m00.11s | DiscreteCategory                      | 0m00.08s
    0m00.10s | Functor/Identity                      | 0m00.11s
    0m00.10s | Category/StrictCategory               | 0m00.08s
    0m00.10s | Peano                                 | 0m00.15s
    0m00.09s | Utf8                                  | 0m00.08s
    0m00.09s | Grothendieck                          | 0m00.08s
    0m00.08s | NaturalTransformation                 | 0m00.10s
    0m00.07s | Functor                               | 0m00.08s
    0m00.06s | Notations                             | 0m00.04s
    0m00.04s | NotationsUtf8                         | 0m00.03s
    -----------------------------------------------------------
    2m25.20s | Total                                 | 2m26.10s

--- theories/NatCategory/Duals.v
+++ theories/NatCategory/Duals.v
@@ -1,4 +1,4 @@
-Require Export Category.Duals NatCategory Category.Equality.
+Require Export Category.Duals NatCategory Discrete.Duals.
 Require Import Common.
 
 Set Implicit Arguments.
@@ -13,25 +13,8 @@ Definition SelfDual_Initial : 0^op = 0 := idpath.
 Definition SelfDual_Terminal : 1^op = 1 := idpath.
 
 (** The nat categories are propositionally self-dual. *)
-(** We need [CardinalityRepresentative] to be [Opaque] so that we pick
-    up the truncation rule for it. *)
-Local Opaque CardinalityRepresentative.
-
-Local Ltac dual_t :=
-  repeat match goal with
-           | _ => reflexivity
-           | [ |- @sigT ?A ?P ] => let t := fresh in
-                                   assert (t : A);
-                                     [
-                                     | exists t ]
-           | _ => progress (repeat (apply path_forall; intro); simpl)
-           | _ => apply (path_universe (symmetry _ _))
-           | _ => exact (center _)
-           | _ => progress category_eq; simpl
-         end.
-
 Lemma SelfDual_NatCategory `{Funext} `{Univalence} (n : nat) : n^op = n.
 Proof.
-  destruct n as [|[|]];
-  dual_t.
+  destruct n as [|[|]]; try reflexivity;
+  apply SelfDual_Discrete.
 Qed.
