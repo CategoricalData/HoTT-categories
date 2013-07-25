8a4425933cb580bdc107088825a461ceb469d8d1
Jason Gross <jgross@mit.edu>
2013-07-25 14:57:28 -0400

    Update coq-load-path

--- theories/.dir-locals.el
+++ theories/.dir-locals.el
@@ -1,4 +1,5 @@
-((coq-mode . ((coq-load-path . (("." "HoTT.Categories") ("../HoTT/theories" "HoTT")))
-	      (eval . (let ((default-directory (locate-dominating-file
+((coq-mode . ((eval . (let ((default-directory (locate-dominating-file
 						buffer-file-name ".dir-locals.el")))
-			(setq coq-prog-name (expand-file-name "../HoTT/hoqtop")))))))
+			(setq coq-prog-name (expand-file-name "../HoTT/hoqtop"))
+			(setq coq-load-path `((,(expand-file-name ".") "HoTT.Categories")
+					      (,(expand-file-name "../HoTT/theories") "HoTT"))))))))
