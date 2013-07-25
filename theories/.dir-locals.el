58902b5296fd841404796f92cd79dac251f69899
Jason Gross <jgross@mit.edu>
2013-07-25 13:58:51 -0400

    Remove extraneous .dir-locals.el
    This is based on the recently merged commit to HoTT.

--- theories/.dir-locals.el
+++ theories/.dir-locals.el
@@ -1,2 +1,4 @@
 ((coq-mode . ((coq-load-path . (("." "HoTT.Categories") ("../HoTT/theories" "HoTT")))
-	      (coq-prog-name . "../HoTT/hoqtop"))))
+	      (eval . (let ((default-directory (locate-dominating-file
+						buffer-file-name ".dir-locals.el")))
+			(setq coq-prog-name (expand-file-name "../HoTT/hoqtop")))))))
--- theories/NaturalTransformation/.dir-locals.el
+++
@@ -1,2 +0,0 @@
-((coq-mode . ((coq-load-path . (".." (".." "HoTT.Categories") ("../../HoTT/theories" "HoTT")))
-	      (coq-prog-name . "../../HoTT/hoqtop"))))
