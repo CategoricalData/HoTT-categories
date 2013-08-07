28e8ffcf31ac359e8b69403482dad79c8010b3bd
Jason Gross <jgross@mit.edu>
2013-08-07 11:22:17 -0400

    Add some instances of isos of NTs
    After    | File Name                             | Before
    -----------------------------------------------------------
    0m18.09s | Category/Equality                     | 0m20.38s
    0m11.79s | Comma/Duals                           | 0m12.04s
    0m10.98s | Groupoid                              | 0m10.30s
    0m09.08s | Comma/InducedFunctors                 | 0m09.19s
    0m06.25s | SumPreCategory                        | 0m04.94s
    0m05.61s | Category/Morphisms                    | 0m05.34s
    0m05.00s | Groupoid/Duals                        | 0m04.84s
    0m04.80s | Functor/Equality                      | 0m05.94s
    0m04.12s | Groupoid/Functors                     | 0m03.94s
    0m03.73s | UniversalProperties                   | 0m03.59s
    0m02.96s | Grothendieck/ToSet                    | 0m03.06s
    0m02.67s | BoolCategory                          | 0m02.78s
    0m02.65s | NaturalTransformation/Duals           | 0m02.18s
    0m02.59s | Comma/CommaCategory                   | 0m02.58s
    0m02.12s | NaturalTransformation/Equality        | 0m02.15s
    0m02.06s | Category/Product                      | 0m01.82s
    0m02.02s | Hom                                   | 0m01.70s
    0m01.70s | NaturalTransformation/Product         | 0m01.40s
    0m01.64s | NaturalTransformation/Composition     | 0m01.60s
    0m01.35s | Functor/Product                       | 0m01.58s
    0m01.30s | NaturalTransformation/CompositionLaws | 0m01.28s
    0m01.18s | FunctorCategory/Morphisms             | 0m00.97s
    0m01.16s | NaturalTransformation/Isomorphisms    | 0m00.12s
    0m00.66s | InitialTerminalCategory               | 0m00.75s
    0m00.52s | Functor/CompositionLaws               | 0m00.51s
    0m00.46s | Common                                | 0m00.48s
    0m00.39s | Comma/Projection                      | 0m00.42s
    0m00.38s | Discrete/Duals                        | 0m00.30s
    0m00.36s | IndiscreteCategory                    | 0m00.38s
    0m00.34s | FunctorCategory                       | 0m00.29s
    0m00.33s | Cat                                   | 0m00.32s
    0m00.32s | SetCategory                           | 0m00.36s
    0m00.32s | Adjoint/UnitCounit                    | 0m00.31s
    0m00.31s | NaturalTransformation/Identity        | 0m00.35s
    0m00.30s | Functor/Composition                   | 0m00.32s
    0m00.28s | Functor/Duals                         | 0m00.27s
    0m00.26s | Category/Objects                      | 0m00.27s
    0m00.23s | NaturalNumbersObject                  | 0m00.15s
    0m00.22s | IsGroupoid                            | 0m00.24s
    0m00.22s | NatCategory                           | 0m00.28s
    0m00.22s | NaturalTransformation/Core            | 0m00.23s
    0m00.18s | ComputableCat                         | 0m00.19s
    0m00.16s | Category/Core                         | 0m00.20s
    0m00.16s | Category/Duals                        | 0m00.16s
    0m00.15s | NatCategory/Duals                     | 0m00.16s
    0m00.13s | Utf8                                  | 0m00.12s
    0m00.13s | Peano                                 | 0m00.13s
    0m00.12s | Category/Category                     | 0m00.12s
    0m00.12s | Functor                               | 0m00.09s
    0m00.12s | Functor/Core                          | 0m00.12s
    0m00.12s | Grothendieck                          | 0m00.10s
    0m00.11s | Functor/Identity                      | 0m00.13s
    0m00.11s | Category/StrictCategory               | 0m00.10s
    0m00.10s | NaturalTransformation                 | 0m00.15s
    0m00.09s | DiscreteCategory                      | 0m00.12s
    0m00.07s | Notations                             | 0m00.07s
    0m00.05s | NotationsUtf8                         | 0m00.04s
    -----------------------------------------------------------
    1m52.89s | Total                                 | 1m51.94s

--- theories/NaturalTransformation/Isomorphisms.v
+++ theories/NaturalTransformation/Isomorphisms.v
@@ -1,1 +1,59 @@
-Require Export FunctorCategory.Morphisms.
+Require Export NaturalTransformation.Core NaturalTransformation.Composition Category.Morphisms FunctorCategory.Morphisms.
+Require Import Common.
+
+Set Universe Polymorphism.
+Set Implicit Arguments.
+Generalizable All Variables.
+Set Asymmetric Patterns.
+
+Local Open Scope category_scope.
+Local Open Scope natural_transformation_scope.
+Local Open Scope morphism_scope.
+
+
+Local Ltac iso_whisker_t :=
+  nt_eq;
+  try rewrite <- FCompositionOf, <- FIdentityOf;
+  try f_ap;
+  match goal with
+    | [ H : IsIsomorphism _ |- appcontext[ComponentsOf ?T0 ?x ∘ ComponentsOf ?T1 ?x] ]
+      => change (T0 x ∘ T1 x) with (ComponentsOf ((T0 : Morphism [_, _] _ _)
+                                                    ∘ (T1 : Morphism [_, _] _ _))%morphism
+                                                 x);
+        progress rewrite ?(@LeftInverse _ _ _ _ H), ?(@RightInverse _ _ _ _ H)
+  end;
+  reflexivity.
+
+Section composition.
+  Context `{Funext}.
+
+  Global Instance iso_NTComposeT
+         `(T' : @NaturalTransformation C D F' F'')
+         `(T : @NaturalTransformation C D F F')
+         `{@IsIsomorphism [C, D] F' F'' T'}
+         `{@IsIsomorphism [C, D] F F' T}
+  : @IsIsomorphism [C, D] F F'' (NTComposeT T' T)
+    := @isisomorphism_composition [C, D] _ _ T' _ _ T _.
+
+  Global Instance iso_NTWhiskerL C D E
+         (F : Functor D E)
+         (G G' : Functor C D)
+         (T : NaturalTransformation G G')
+         `{@IsIsomorphism [C, D] G G' T}
+  : @IsIsomorphism [C, E] (F ∘ G)%functor (F ∘ G')%functor (NTWhiskerL F T).
+  Proof.
+    exists (NTWhiskerL F (T : Morphism [_, _] _ _)^-1);
+    abstract iso_whisker_t.
+  Defined.
+
+  Global Instance iso_NTWhiskerR C D E
+         (F F' : Functor D E)
+         (T : NaturalTransformation F F')
+         (G : Functor C D)
+         `{@IsIsomorphism [D, E] F F' T}
+  : @IsIsomorphism [C, E] (F ∘ G)%functor (F' ∘ G)%functor (NTWhiskerR T G).
+  Proof.
+    exists (NTWhiskerR (T : Morphism [_, _] _ _)^-1 G);
+    abstract iso_whisker_t.
+  Defined.
+End composition.
