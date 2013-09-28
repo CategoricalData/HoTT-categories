05170e3804252e0ef55afecaaf390ea12e6212d6
Jason Gross <jgross@mit.edu>
2013-09-28 11:27:16 -0400

    Fix .dir-locals with new directory structure

--- theories/.dir-locals.el
+++ theories/.dir-locals.el
@@ -1,5 +1,5 @@
 ((coq-mode . ((eval . (let ((default-directory (locate-dominating-file
 						buffer-file-name ".dir-locals.el")))
 			(setq coq-prog-name (expand-file-name "../HoTT/hoqtop"))
-			(setq coq-load-path `((,(expand-file-name ".") "HoTT.Categories")
+			(setq coq-load-path `((,(expand-file-name ".") "HoTT")
 					      (,(expand-file-name "../HoTT/theories") "HoTT"))))))))
